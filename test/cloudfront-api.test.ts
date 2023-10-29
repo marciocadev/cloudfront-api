1b56b136cf1db3762bed57163346bc79b5b3fba8
Marcio Cruz de Almeida <marciocadev@gmail.com>
2023-10-29 19:57:22 -0300

    Initial commit

---
+++ test/cloudfront-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CloudfrontApi from '../lib/cloudfront-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cloudfront-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CloudfrontApi.CloudfrontApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
